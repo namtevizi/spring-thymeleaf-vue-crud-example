e8f67d68d75821c7dff7a97552914b7ba44a77dd
Nam Tran <namtvh@gmail.com>
2022-05-13 19:43:14 +0700

    Spring Boot Thymeleaf VueJS Embbded in backend

---
+++ backend/src/main/java/dev/drugowick/springthymeleafvuecrudexample/web/api/v1/RolesController.java
@@ -0,0 +1,33 @@
+package dev.drugowick.springthymeleafvuecrudexample.web.api.v1;
+
+import dev.drugowick.springthymeleafvuecrudexample.domain.Role;
+import dev.drugowick.springthymeleafvuecrudexample.infrastructure.RoleRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RolesController {
+
+    private final RoleRepository roleRepository;
+
+    public RolesController(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping("roles")
+    public List<Role> list() {
+        return roleRepository.findAll();
+    }
+
+    @PostMapping("roles")
+    public Role save(@RequestBody Role role) {
+        return roleRepository.save(role);
+    }
+
+    @DeleteMapping("roles/{id}")
+    public void delete(@PathVariable Long id) {
+        roleRepository.deleteById(id);
+    }
+}
---
+++ backend/src/main/java/dev/drugowick/springthymeleafvuecrudexample/web/api/v1/UsersController.java
@@ -0,0 +1,52 @@
+package dev.drugowick.springthymeleafvuecrudexample.web.api.v1;
+
+import dev.drugowick.springthymeleafvuecrudexample.domain.Role;
+import dev.drugowick.springthymeleafvuecrudexample.domain.User;
+import dev.drugowick.springthymeleafvuecrudexample.infrastructure.RoleRepository;
+import dev.drugowick.springthymeleafvuecrudexample.infrastructure.UserRepository;
+import dev.drugowick.springthymeleafvuecrudexample.web.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UsersController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UsersController(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User save(@RequestBody @NotNull UserDto userDto) {
+        Optional<Role> role = roleRepository.findById(userDto.getRoleId());
+
+        if (role.isPresent()) {
+            User user = new User();
+            if (userDto.getId() != null) user.setId(userDto.getId());
+            user.setFirstName(userDto.getFirstName());
+            user.setLastName(userDto.getLastName());
+            user.setRole(role.get());
+
+            return userRepository.save(user);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
